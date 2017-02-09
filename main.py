34ae62b34197ff1589d9db8694d998f4120f8b74
tszho <tszho@dyn1221-26.wlan.ic.ac.uk>
2017-02-09 14:41:11 +0000

    main

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import time
 from machine import I2C,Pin
 import change,ambient,proximity
 
-print('shit')
+print('START')
 
 bus = I2C(scl = Pin(5),sda = Pin(4),freq=100000)
 
@@ -11,12 +11,20 @@ deviceAddr = 19
 proximity.set(bus,deviceAddr)
 
 i = 1
-buf = bytearray(2)
+buf = bytearray(4)
+lum = bytearray(2)
+pro = bytearray(2)
 
 while True:
 	buf = proximity.read(bus,deviceAddr)
-	print(buf)
-	i +=1
-	time.sleep(0.5)
+#	print(buf)
+	
+	lum = buf[0]*256 +buf[1]
+	pro = buf[2]*256 +buf[3]
+	
+
+	print ("Ambient Light Luminance: %d lux" %lum)
+	print ("Proximity of the Device: %d" %pro)
+	time.sleep(0.8)
 
 print('fin')
