4a767f35a6ded8c8374b159c310771a736bd1400
Max <mcdick958@gmail.com>
2017-02-15 22:59:09 +0000

    serverside

--- serverside/main.py
+++ serverside/main.py
@@ -15,19 +15,29 @@ def on_connect(client, userdata, flags, rc):
 
 # The callback for when a PUBLISH message is received from the server.
 def board1_callback(client, userdata, msg):
-	global i		
+	global board,i,chesstype
 	print(msg.topic+" "+str(msg.payload))	
 	data = "data recieved from board, %d" %i + "th iteration"
 	time.sleep(0.5)
 	i = i + 1
 	msgJ = json.loads(str(msg.payload))
-	print(msgJ['Hue'])
-	hue = msgJ['Hue']
-	
+	print("t45" + str(msgJ['t45']))
+	print("t46" + str(msgJ['t46']))
+
+	if msgJ['t45'] != "U":
+		board.removepiece("t45")
+		board.addpiece("t45",chesstype[msgJ['t45']],4,5)
+	else:
+		board.removepiece("t45")
+	if msgJ['t46'] != "U":
+		board.removepiece("t46")
+		board.addpiece("t46",chesstype[msgJ['t46']],4,6)
+	else:
+		board.removepiece("t46")
+
 	DATA = {
 	    "Name": 'Server',
-	    "iteration": i,
-	    "1c": 1
+	    "t11": 1
 	}
 	board.update()
 	client.publish("esys/ATeam/ServerToBoard",json.dumps(DATA))
@@ -36,25 +46,44 @@ def _main(event):
 	global client
 	global board
 	print event.x, event.y
-	board.removepiece("player0")
-'''
+
 	client = mqtt.Client()
 	client.on_connect = on_connect
 	client.connect("192.168.0.10", 1883, 60)
 	client.loop_forever()
-'''
-i = 0
+if __name__ == "__main__":
 
-root = tk.Tk()
-board = GameBoard(root)
-pwnW = tk.PhotoImage(file = "pawnW.gif")
-pwnB = tk.PhotoImage(file = "pawnB.gif")
-for i in range(0,8):
-	#board.addpiece("player%d"%i, pwnW, 1,i)
-	board.addpiece("player%d"%(i+8), pwnB, 6,i)
+	i = 0
+	#setting up the board
+	root = tk.Tk()
+	board = GameBoard(root)
+	
+	chesstype = {}
+	chesstype["R"] = tk.PhotoImage(file = "queenB.gif")
+	chesstype["G"] = tk.PhotoImage(file = "kingB.gif")
+	chesstype["B"] = tk.PhotoImage(file = "bishopB.gif")
+	chesstype["Y"] = tk.PhotoImage(file = "knightB.gif")
+	chesstype["O"] = tk.PhotoImage(file = "rookB.gif")
+	chesstype["C"] = tk.PhotoImage(file = "pawnB.gif")
 
-board.pack(side="top", fill="both", expand="true", padx=4, pady=4)
-root.bind("<Button-1>", _main)
+	chesstype["R2"] = tk.PhotoImage(file = "queenW.gif")
+	chesstype["G2"] = tk.PhotoImage(file = "kingW.gif")
+	chesstype["B2"] = tk.PhotoImage(file = "bishopW.gif")
+	chesstype["Y2"] = tk.PhotoImage(file = "knightW.gif")
+	chesstype["O2"] = tk.PhotoImage(file = "rookW.gif")
+	chesstype["C2"] = tk.PhotoImage(file = "pawnW.gif")
 
-root.mainloop()
+	for i in range(0,8):
+		board.addpiece("player%d"%i, chesstype["C2"], 1,i)
+		board.addpiece("player%d"%(i+8), chesstype["C"], 6,i)
+	
+	empty = tk.PhotoImage(file = "Blank.gif")
+	board.addpiece("t45",empty,4,5)
+	board.addpiece("t46",empty,4,6)
+
+	board.pack(side="top", fill="both", expand="true", padx=4, pady=4)
+	root.bind("<Button-3>", _main)
+
+	#entering the GUI loop
+	root.mainloop()
 
