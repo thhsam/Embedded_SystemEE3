679df5c68459614630e2c341e8ceddf838f9071c
tszho <tszho@dyn1223-48.wlan.ic.ac.uk>
2017-02-15 20:22:16 +0000

    main

--- main.py
+++ main.py
@@ -89,22 +89,38 @@ ss = s[28]+s[29]
 client.disconnect() 
 client.connect()
 
+bus2 = I2C(scl = Pin(5),sda = Pin(16),freq=100000)
 bus = I2C(scl = Pin(5),sda = Pin(4),freq=100000)
+
 deviceAddr = 19
 colordeviceAddr = 41
 
 VCNL4010.init_all(bus,deviceAddr)
 TCS3472.set(bus,colordeviceAddr)
 
+VCNL4010.init_all(bus2,deviceAddr)
+TCS3472.set(bus2,colordeviceAddr)
+
 buf = bytearray(4)
 lum = bytearray(2)
 pro = bytearray(2)
+buf1 = bytearray(4)
+lum1 = bytearray(2)
+pro1 = bytearray(2)
 colour = ""
+colour = ""
+
 
 bufrgb = bytearray(6)
 red = bytearray(2)
 green = bytearray(2)
 blue = bytearray(2)
+
+bufrgb1 = bytearray(6)
+red1 = bytearray(2)
+green1 = bytearray(2)
+blue1 = bytearray(2)
+
 
 rtc=machine.RTC()
 rtc.datetime((int(year), int(month), int(day),0, int(hh), int(mm), int(ss), 0))
@@ -115,18 +131,31 @@ while True:
 	lum = buf[0]*256 +buf[1]
 	pro = buf[2]*256 +buf[3]
 	
-        bufrgb = TCS3472.readRGB(bus, colordeviceAddr)
-        red = bufrgb[0] + bufrgb[1]*256
-        green = bufrgb[2] + bufrgb[3]*256
-        blue = bufrgb[4] + bufrgb[5]*256
+    buf1 = VCNL4010.read_all(bus2,deviceAddr)
+    lum1 = buf1[0]*256 + buf1[1]
+    pro1 = buf1[2]*256 +buf1[3]
+    
+    bufrgb = TCS3472.readRGB(bus, colordeviceAddr)
+    red = bufrgb[0] + bufrgb[1]*256
+    green = bufrgb[2] + bufrgb[3]*256
+    blue = bufrgb[4] + bufrgb[5]*256
+        
+    bufrgb1 = TCS3472.readRGB(bus2, colordeviceAddr)
+    red1 = bufrgb1[0] + bufrgb1[1]*256
+    green1 = bufrgb1[2] + bufrgb1[3]*256
+    blue1 = bufrgb1[4] + bufrgb1[5]*256
 
 	hsvR = float(red * 1.0)
 	hsvG = float(blue* 1.0)
 	hsvB = float(green*1.0)
+    
+    hsvR1 = float(red1 * 1.0)
+    hsvG1 = float(blue1* 1.0)
+    hsvB1 = float(green1*1.0)
 
 	if hsvR == hsvB == hsvG:
 		hsv = 0
-	elif hsvR < hsvG and hsvR < hsvB:
+	elif hsvR < hsvG and hsvR < hsvB:
 		hsv = 120 +  120*(hsvB - hsvR)/((hsvG - hsvR) + (hsvB - hsvR))
 	elif hsvG < hsvB and hsvG < hsvR:
 		hsv = 240 + 120*(hsvR - hsvG)/((hsvR - hsvG) + (hsvB - hsvG))
@@ -157,12 +186,12 @@ while True:
         t = rtc.datetime()
         
         print ("Red: %d" %red + " Green: %d" %green + " Blue: %d" %blue + " Hue: %d" %hsv)
-	print ("Luminance: %d lux" %lum)
-	print ("Proximity: %d" %pro)
+        print ("Luminance: %d lux" %lum)
+        print ("Proximity: %d" %pro)
         print ("Time: %d-" %t[0] + "%d-" %t[1] + "%d" %t[2], "%d:" %t[4], "%d:" %t[5], "%d" %t[6])
         print ("Colour: %s" %colour)
 
-	data = "{\"Time\": %d-" %t[0] + "%d-" %t[1] + "%d " %t[2] + "%d" %t[4] + ":%d" %t[5] + ":%d, " %t[6] + "\"Luminance\": %d, " %lum + "\"Proximity\": %d, " %pro + "\"Hue\": %d }" %hsv 
+        data = "{\"Time\": %d-" %t[0] + "%d-" %t[1] + "%d " %t[2] + "%d" %t[4] + ":%d" %t[5] + ":%d, " %t[6] + "\"Luminance\": %d, " %lum + "\"Proximity\": %d, " %pro + "\"Hue\": %d }" %hsv
 
         #Put Data into JSON
         DATA = {
@@ -174,7 +203,7 @@ while True:
         }
 
         #Publish data over MQTT
-	#client.publish('esys/ATeam/BoardToServer', bytes(data, 'utf-8'))
+        #client.publish('esys/ATeam/BoardToServer', bytes(data, 'utf-8'))
         json_str = ujson.dumps(DATA)
         client.publish('esys/ATeam/BoardToServer', bytes(json_str, 'utf-8'), qos=1)
 
